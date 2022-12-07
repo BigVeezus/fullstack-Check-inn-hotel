8ad42f5637244e0380af863920be76a4e90c6f6a
ELVIS <user@ELVISs-MBP.lan>
2022-12-07 11:45:43 +0100

    added node modules

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send("WOOF");
+});
+
 app.listen(port, () => {
   console.log("PORT NOW LISTENING ON 3000 G!");
 });
