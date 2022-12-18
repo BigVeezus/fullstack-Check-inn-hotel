651294ed06516984cd24bb6d14abcd15bee56e3a
ELVIS <elvis.osujic@gmail.com>
2022-12-18 02:12:14 +0100

    Handling every error

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const path = require("path");
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
+const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
 const Hotel = require("./model/hotel");
 
 mongoose.connect("mongodb://localhost:27017/check-inn", {
@@ -29,44 +31,73 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/hotels", async (req, res) => {
-  const hotels = await Hotel.find({});
-  res.render("hotels/index", { hotels });
-});
+app.get(
+  "/hotels",
+  catchAsync(async (req, res) => {
+    const hotels = await Hotel.find({});
+    res.render("hotels/index", { hotels });
+  })
+);
 
 app.get("/hotels/new", (req, res) => {
   res.render("hotels/new");
 });
 
-app.post("/hotels", async (req, res) => {
-  const newHotel = new Hotel(req.body.hotel);
-  await newHotel.save();
-  console.log(newHotel);
-  res.redirect(`/hotels/${newHotel._id}`);
+app.post(
+  "/hotels",
+  catchAsync(async (req, res, next) => {
+    if (!req.body.hotel) throw new ExpressError("Invalid Hotel Data", 404);
+    const newHotel = new Hotel(req.body.hotel);
+    await newHotel.save();
+    console.log(newHotel);
+    res.redirect(`/hotels/${newHotel._id}`);
+  })
+);
+
+app.get(
+  "/hotels/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const hotel = await Hotel.findById(id);
+    res.render("hotels/show", { hotel });
+  })
+);
+
+app.get(
+  "/hotels/:id/edit",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const hotel = await Hotel.findById(id);
+    res.render("hotels/edit", { hotel });
+  })
+);
+
+app.put(
+  "/hotels/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const hotel = await Hotel.findByIdAndUpdate(id, { ...req.body.hotel });
+    res.redirect(`/hotels/${hotel._id}`);
+  })
+);
+
+app.delete(
+  "/hotels/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Hotel.findByIdAndDelete(id);
+    res.redirect("/hotels");
+  })
+);
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found!", 404));
 });
 
-app.get("/hotels/:id", async (req, res) => {
-  const { id } = req.params;
-  const hotel = await Hotel.findById(id);
-  res.render("hotels/show", { hotel });
-});
-
-app.get("/hotels/:id/edit", async (req, res) => {
-  const { id } = req.params;
-  const hotel = await Hotel.findById(id);
-  res.render("hotels/edit", { hotel });
-});
-
-app.put("/hotels/:id", async (req, res) => {
-  const { id } = req.params;
-  const hotel = await Hotel.findByIdAndUpdate(id, { ...req.body.hotel });
-  res.redirect(`/hotels/${hotel._id}`);
-});
-
-app.delete("/hotels/:id", async (req, res) => {
-  const { id } = req.params;
-  await Hotel.findByIdAndDelete(id);
-  res.redirect("/hotels");
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Oh no, Something went wrong!";
+  res.status(statusCode).render("error", { err });
 });
 
 app.listen(port, () => {
